15f6dadd0d0bb278810557a8928a0902b1bca73e
billzhou2008 <billzhou2005@gmail.com>
2021-11-01 16:48:39 +0800

    init

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,19 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h2>{{ store.state.counter }}</h2>
+    <h1>Show Counter as global value</h1>
   </div>
 </template>
+
+<script>
+import { inject } from 'vue'
+export default {
+  setup() {
+    const store = inject('store')
+
+    return {
+      store
+    }
+  }
+}
+</script>
